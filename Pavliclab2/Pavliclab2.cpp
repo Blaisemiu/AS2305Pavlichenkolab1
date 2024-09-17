065b72698ba6414631a62ca37701a462e6a365c0
Yaroslava <pavlichenkoyaroslava777@gmail.com>
2024-09-17 19:57:27 +0300

    Creating a Pipe structure

---
+++ Pavliclab2/Pavliclab2.cpp
@@ -0,0 +1,15 @@
+﻿// Pavliclab2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct Pipe
+{
+	string name;
+	int length;
+	int diameter;
+	bool sing;
+};
+
